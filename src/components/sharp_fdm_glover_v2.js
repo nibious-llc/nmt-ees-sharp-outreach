f97ff4207a524bbaccb92b8da58714b6368477ff
Owen Parkins <oparkins@gmail.com>
2020-05-25 11:26:36 -0600

    Converted the Sharp Interface code to Javascript. Added tests to ensure that the code returns the right values under standard circumstances.

---
+++ src/components/sharp_fdm_glover_v2.js
@@ -0,0 +1,271 @@
+import { lusolve, matrix, zeros} from 'mathjs';
+
+
+
+function initIterationLoop(Nx, delx) {
+	let xx = 0.0;
+	const x = new Array(Nx)
+	const h = new Array(Nx)
+	const z = new Array(Nx)
+	for (let i = 0; i < Nx; i++) {
+		x[i] = xx;
+		xx = xx+delx;
+		h[i] = 0.0;
+		z[i] = -50.0;
+	}
+	return [x, h, z];
+}
+
+function calculateTransmissivities(h, z, Nx, Kf, Ks) {
+	let Tf = new Array(Nx);
+	let Ts = new Array(Nx);
+	for (let n = 0; n < Nx; n++) {
+		let bf = h[n] - z[n];
+		let bs = z[n] + 500;
+		Tf[n] = bf*Kf;		
+		Ts[n] = bs*Ks;
+	}
+	return [Tf, Ts];
+}
+
+/**
+ * Validates All User Input
+ * @param {double} delx m: controls size of grid; perhaps vary between 30m to 120m
+ * @param {double} k permeability (m^2): controls permeability of aquifer, higher perm leads to lower water table perhaps constrain between 10^-10 to10^-14 m2
+ * @param {double} Rech m/day: contrains amount of water replenishing the aquifer; recharge can't be zero 
+ * @param {double} Qp Pumping rate (m^3/day/island length) The higher Qp, the lower the water table falls around the pumpming node nQp. If pumping rate is set too high, the code blows up. 
+ * @param {int} nQp node where pumping occurs from. Shoud be constrated to be between nodes 10-90
+ */
+function validateUserInput(delx, k, Rech, Qp, nQp) {
+	if (delx < 30 || delx > 120) {
+		return false;
+	}
+
+	if(k > Math.pow(10, -10) || k < Math.pow(10, -14)) {
+		return false;
+	}
+
+	if(Rech <= 0) {
+		return false;
+	}
+
+	if(nQp < 10 || nQp > 90) {
+		return false;
+	}
+	return true;
+}
+
+
+
+/**
+ * Calculate the Sharp Interface with given inputs
+ * @param {double} delx m: controls size of grid; perhaps vary between 30m to 120m
+ * @param {double} k permeability (m^2): controls permeability of aquifer, higher perm leads to lower water table perhaps constrain between 10^-10 to10^-14 m2
+ * @param {double} Rech m/day: contrains amount of water replenishing the aquifer; recharge can't be zero 
+ * @param {double} Qp Pumping rate (m^3/day/island length) The higher Qp, the lower the water table falls around the pumpming node nQp. If pumping rate is set too high, the code blows up. 
+ * @param {int} nQp node where pumping occurs from. Shoud be constrated to be between nodes 10-90
+ */
+export default function SharpInterface(delx=60, k = 1.0e-11, Rech = 0.009, Qp = 0.35, nQp = 10) {
+
+
+	if (validateUserInput(delx, k, Rech, Qp, nQp) == false) {
+		return false;
+	}
+
+	const Nx = 101;  			// number nodes, do not change.
+	//let delx = 60;   			// (m) MAP: controls size of grid; perhaps vary between 30m to 120m
+  //let k = 1.0e-11; 			// permeability (m^2) MAP controls permeability of aquifer, 
+												// higher perm leads to lower water table perhaps constrain 
+												// between 10^-10 to10^-14 m2
+  const rho_f = 1000.0; // kg/m^3 do not change
+  const rho_s = 1025.0; // kg/m^3 do not change
+  const vis_f = 0.001;  // Pa-s  do not change
+  const vis_s = 0.0012; // Pa-s  do not change
+  //let Rech = 0.009;    	// m/day MAP: contrains amount of water replenishing the aquifer; recharge can't be zero 
+  const grav = 9.81;  	// m/s^2  do not change
+  //let Qp = 0.35; 				// MAP Pumping rate (m^3/day/island length) The higher Qp, the lower the water table falls
+												// around the pumpming node nQp. If pumping rate is set too high, the code
+												// blows up. 
+	//let nQp = 10;         // MAP node where pumping occurs from. Shoud be constrated to be between nodes 10-90
+	const Iter = 10;      // number of iterations to find the stable solution. We could potentially change this 
+		   									// to illstrate the non-linear nature of the solution. 
+
+
+	const Kf = k*rho_f*grav/vis_f*3600*24;
+	const Ks = k*rho_s*grav/vis_s*3600*24;
+
+	const qf_glover = matrix();
+
+	const D = rho_f/rho_s;
+	const Rr = (rho_s-rho_f)/rho_s;
+	
+	const [x, h, z] = initIterationLoop(Nx, delx);
+	const hh = matrix();
+	const zz = matrix();
+
+	for (let it = 0; it < Iter; it++) {
+
+		// calculate Transmissivities
+		const [Tf, Ts] = calculateTransmissivities(h, z, Nx, Kf, Ks);
+
+		// assign spatially varying FDM parameters
+		let Bfp = new Array(Nx)
+		let Bfm = new Array(Nx)
+		let Bsp = new Array(Nx)
+		let Bsm = new Array(Nx)
+		for (let n = 1; n < Nx - 1; n++) {
+			Bfp[n] = (Tf[n]+ Tf[n+1])/2;
+			Bfm[n] = (Tf[n]+ Tf[n-1])/2;
+			Bsp[n] = (Ts[n]+ Ts[n+1])/2;
+			Bsm[n] = (Ts[n]+ Ts[n-1])/2;
+		}
+
+		Bfp[0] = (Tf[Nx - 2]+ Tf[Nx - 1])/2; 
+    Bfm[0] = Tf[0];
+    Bsp[0] = (Ts[Nx - 2] + Ts[Nx - 1])/2;
+    Bsm[0] = Ts[0];
+
+		Bfp[Nx - 1] = Tf[Nx - 1];
+		Bfm[Nx - 1] = (Tf[Nx - 1] + Tf[Nx - 2])/2;
+		Bsp[Nx - 1] = Ts[Nx - 1];
+		Bsm[Nx - 1] = (Ts[Nx - 1] + Ts[Nx - 2])/2;
+		
+		// zero A matrix and B vector
+
+		const Nx2 = 2 * Nx - 6;
+
+		const a = matrix(zeros(Nx2, Nx2))
+		const b = matrix(zeros(Nx2, 1))
+	
+		// populate first two unknowns associated with boundary conditions
+		a.set([0, 0], Bfm[1]+Bfp[1]);
+		a.set([0, 1], 0);
+		a.set([0, 2], -1 * Bfp[1]);
+		b.set([0, 0], Bfm[1]*h[0] + Rech*Math.pow(delx, 2));
+		
+		a.set([1,0], D*(Bsm[1]+ Bsp[1]));
+		a.set([1,1], Rr*(Bsm[1]+Bsp[1]));
+		a.set([1,2], -1 * D*Bsp[1]);
+		a.set([1,3], -1 * Rr*Bsp[1]);
+		b.set([1,0], D*Bsm[1]*h[0]+Rr*Bsm[1]*z[0]);
+
+		// populate a matrix with fdm coefficients
+		// for freshwater transport equation
+
+		let ic = 2;
+		for(let n = 2; n < Nx2; n = n + 2) {
+			a.set([n, n], Bfm[ic]+Bfp[ic]);
+			a.set([n, n-2], -1 * Bfm[ic]);
+			a.set([n, n-1], 0);
+			a.set([n, n+1], 0);
+			a.set([n, n+2], -1 * Bfp[ic]);
+			b.set([n, 0], Rech*Math.pow(delx, 2));
+
+			if (ic== nQp - 1) {
+				b.set([n, 0], b.get([n,0]) - Qp*Math.pow(delx, 2));
+			}
+			ic = ic+1;
+		}
+
+		ic = 2;
+		for (let n = 3; n < Nx2; n = n + 2) {
+			a.set([n,n],   Rr*(Bsm[ic]+Bsp[ic]));
+			a.set([n,n-3],  -1 * D*Bsm[ic]);
+			a.set([n,n-2], -1 * Rr*Bsm[ic]);
+			a.set([n,n-1],  D*(Bsm[ic]+Bsp[ic]));
+			a.set([n,n+1],  -1 * D*Bsp[ic]);
+			a.set([n,n+2], -1 * Rr*Bsp[ic]);
+			b.set([n,0], 0);
+			ic = ic+1;	
+		}
+
+		// populate a matrix and b vector for last two unknowns
+		{
+			let n = 196;
+			ic = 99;
+
+			a.set([n, n], Bfm[ic]+Bfp[ic]);
+			a.set([n, n+1], 0);
+			a.set([n, n-2], -Bfm[ic]);
+			a.set([n, n-1], 0);
+			b.set([n, 0], Rech*Math.pow(delx, 2)+Bfp[ic]*h[Nx - 1]);
+		}
+		
+		{
+			let n = 197;
+			ic = 99;
+			
+			a.set([n,n], Rr*(Bsm[ic]+ Bsp[ic]));
+			a.set([n,n-1], D*(Bsm[ic]+Bsp[ic]));
+			a.set([n,n-2], -Rr*Bsm[ic]);
+			a.set([n,n-3], -D*Bsm[ic]);
+			b.set([n,0], D*Bsp[ic]*h[Nx - 1]+Rr*Bsp[ic]*z[Nx - 1]);
+		}
+
+		/*for(let n = 2; n < 4; n++) {
+			for(let j = 0; j < 4; j++) {
+				console.log(n + "/" + j+ ": " + a.get([n,j]));
+			}
+		}*/
+
+		//hz =a\b;
+		const hz = lusolve(a, b);
+
+		{
+			ic = 1;
+			for (let n = 0; n < Nx2+ 2; n = n + 2) {
+				h[ic] = hz.get([n,0]);
+				z[ic] = hz.get([n + 1,0]);
+				ic=ic+1;
+			}
+		}
+
+		for(let n = 0; n < Nx; n++) {
+			hh.set([n, it], h[n]);
+			zz.set([n, it], z[n]);
+		}
+
+		qf_glover.set([it, 0], rho_f*Tf[0] *(h[1]-h[0])/Kf/(rho_s-rho_f)/delx);
+	 
+		z[0] = 0 -  qf_glover.get([it, 0]);
+		z[Nx - 1] = 0 -  qf_glover.get([it, 0]);
+	}
+
+	return [hh, zz]
+/*
+   % position of interface is controlled by this flux calculation
+   % if there is less recharge, the interface will approach the elevation
+   % of the water table (h=0 at the two sides of the model)
+   
+	
+
+
+/*
+figure(1)
+
+for it=1:Iter
+plot(x,hh(:,it));
+hold on
+plot(x,zz(:,it));
+pause(0.1)
+end
+
+figure(2)
+plot(x,hh(:,Iter));  % plot final iteration
+hold on
+plot (x,zz(:,Iter))
+% end
+
+
+xlabel('Distance (m)')
+ylabel(' Elevation (m)') 
+legend('water table elev.(m)','saltwater-freshwater interface')
+*/ 
+
+
+}
+
+
+if(process.env.NODE_ENV == "test") {
+	module.exports =  {SharpInterface,initIterationLoop, validateUserInput, calculateTransmissivities}
+} 
---
+++ src/components/sharp_fdm_glover_v2.test.js
@@ -0,0 +1,54 @@
+import * as SharpInterface from './sharp_fdm_glover_v2';
+
+
+describe("Testing Sharp Interface", () => {
+	it.each([30, 40, 60, 80, 120])("initIterationLoop return proper h values", (delx) => {
+		const [x, h, z] = SharpInterface.initIterationLoop(101, delx);
+		for (let i = 0; i < h.length; i++) {
+			expect(h[i]).toBe(0);
+		}
+	})
+
+	it.each([30, 40, 60, 80, 120])("initIterationLoop return proper z values", (delx) => {
+		const [x, h, z] = SharpInterface.initIterationLoop(101, delx);
+		for (let i = 0; i < z.length; i++) {
+			expect(z[i]).toBe(-50.0);
+		}
+	})
+
+	it.each([60])("initIterationLoop return proper x values", (delx) => {
+		const [x, h, z] = SharpInterface.initIterationLoop(101, delx);
+		const goldValues = [0, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020, 1080, 1140, 1200, 1260, 1320, 1380, 1440, 1500, 1560, 1620, 1680, 1740, 1800, 1860, 1920, 1980, 2040, 2100, 2160, 2220, 2280, 2340, 2400, 2460, 2520, 2580, 2640, 2700, 2760, 2820, 2880, 2940, 3000, 3060, 3120, 3180, 3240, 3300, 3360, 3420, 3480, 3540, 3600, 3660, 3720, 3780, 3840, 3900, 3960, 4020, 4080, 4140, 4200, 4260, 4320, 4380, 4440, 4500, 4560, 4620, 4680, 4740, 4800, 4860, 4920, 4980, 5040, 5100, 5160, 5220, 5280, 5340, 5400, 5460, 5520, 5580, 5640, 5700, 5760, 5820, 5880, 5940, 6000];
+		for (let i = 0; i < h.length; i++) {
+			expect(x[i]).toBe(goldValues[i]);
+		}
+	})
+
+	it("return proper values", () => {
+		const [hh, zz] = SharpInterface.SharpInterface();
+		expect(zz.get([0,0])).toBe(-50);
+		expect(zz.get([0,1])).toBeCloseTo(-35.96103749009004, 5)
+		expect(zz.get([0,2])).toBeCloseTo(-19.26155335475096, 5)
+		expect(zz.get([0,3])).toBeCloseTo(-16.02823424898521, 5)
+		expect(zz.get([0,4])).toBeCloseTo(-12.8300021077386, 5)
+		expect(zz.get([0,5])).toBeCloseTo(-12.04636481656879, 5)
+		expect(zz.get([0,6])).toBeCloseTo(-10.36773552314449, 5)
+		expect(zz.get([0,7])).toBeCloseTo(-9.951880278525037, 5)
+		expect(zz.get([0,8])).toBeCloseTo(-8.984052256468724, 5)
+		expect(zz.get([0,9])).toBeCloseTo(-8.706577917640544, 5)
+
+		expect(hh.get([1,0])).toBeCloseTo(1.078831124702701, 5);
+		expect(hh.get([1,1])).toBeCloseTo(0.8034342735547727, 5);
+		expect(hh.get([1,2])).toBeCloseTo(1.248204178067895, 5);
+		expect(hh.get([1,3])).toBeCloseTo(1.200693904434692, 5);
+		expect(hh.get([1,4])).toBeCloseTo(1.408382249131064, 5);
+	})
+
+
+	it("calculateTransmissivities return proper values", () => {
+		const [x, h, z] = SharpInterface.initIterationLoop(101, 60);
+		const [Tf, Ts] = SharpInterface.calculateTransmissivities(h, z, 101, 8.475840000000002, 7.239780000000001);
+		expect(Tf[0]).toBe(423.7920000000001);
+	})
+});
+
